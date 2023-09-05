5d1fa34cdec1f9a11a849be569bfc37c3eaecc09
ChurchillV <vinchurch70@gmail.com>
2023-09-05 21:36:45 +0000

    Update ignored files

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import { Route, Routes } from "react-router-dom";
+import { Login, Signup } from "./pages";
+import Home from "./pages/Home";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element = { <Home />} />
+        <Route path="/login" element = { <Login />} />
+        <Route path="/signup" element = { <Signup />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
